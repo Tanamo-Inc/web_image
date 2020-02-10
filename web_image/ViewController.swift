ae71a6426d86112b7072819fb4a8fa3fb4353f77
Anthony <tanamoinc@gmail.com>
2020-02-10 12:15:14 +0000

    Initial Commit

---
+++ web_image/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  web_image
+//
+//  Created by mac on 2/10/20.
+//  Copyright © 2020 tanacom limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
