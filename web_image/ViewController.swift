46306d6a6519a8de933aa64043edda8e9ece6642
Tanamo_Inc <tanamoinc@gmail.com>
2020-02-10 15:17:23 +0000

    async image downloader with cache support.

--- web_image/ViewController.swift
+++ web_image/ViewController.swift
@@ -7,15 +7,27 @@
 //
 
 import UIKit
+import SDWebImage
+
+// Use pod init to initialize a pod file and pod install to install the pod file.
+//This library provides an async image downloader with cache support. For convenience, we added categories
+//for UI elements like UIImageView, UIButton, MKAnnotationView.
+
 
 class ViewController: UIViewController {
     
-    
-// Use pod init to initialize a pod file and pod install to install the pod file.
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        let imageUrl: NSURL? = NSURL(string:"https://lh3.googleusercontent.com/9BNndAU-wAcri56lIYOuNCwC4JX1m5_2UEOo4d3Ql-ecSPy7HSI-rqfipwPtWzbHghE=w2247-h1264")
+        
+        if let url = imageUrl{
+            imageView.sd_setImage(with: url as URL)
+        }
+        
     }
 
 
